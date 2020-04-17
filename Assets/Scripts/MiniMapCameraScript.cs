b93188991c9df2b57dcf1fdf4f6d572c5f77ef9e
Maria Deslis <mdeslis@gmail.com>
2020-04-17 02:08:50 -0600

    added minimap functionality

---
+++ Assets/Scripts/MiniMapCameraScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapCameraScript : MonoBehaviour
+{
+
+	public Transform player; //public reference to ourplayer
+
+	// called after Update and FixedUpdate
+	// only Update after our player has moved
+	// use something similar for sun and moon 
+
+	void LateUpdate()
+	{
+		Vector3 newPosition = player.position; // gra the current player position that has changed
+		newPosition.y = transform.position.y; // change our current y position in the minimap to our new y position for the camera
+		transform.position = newPosition; // now the positions match up
+
+		// rotate minimap with the player
+		transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+
+	}
+}
