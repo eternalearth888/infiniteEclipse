86addc250472dca5e11ba3e9f963f2e4c9cdc716
Maria Deslis <mdeslis@gmail.com>
2020-04-25 20:10:40 -0600

    started making scene switch code, fixed crosshair for all scenes

---
+++ Assets/Scripts/sceneLevelTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneLevelTrigger : MonoBehaviour
+{
+	public string nextScene;
+
+	void onTriggerEnter(Collider Other)  {
+    	// Load the next scene in the build index
+    	SceneManager.LoadScene(nextScene);
+    }
+}
