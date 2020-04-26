f238c49179d9683f8c85949a506eec84ef5fe328
Maria Deslis <mdeslis@gmail.com>
2020-04-25 22:09:15 -0600

    added intro dialogue

---
+++ Assets/Scripts/showTextDialogue.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showTextDialogue : MonoBehaviour
+{
+    public GameObject uiObject;
+
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "player")
+        {
+        	Debug.Log("OnTriggerEnter Activated");
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+
+    // Destroy the gameobject so they don't walk over it multiple times
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(2);
+        Destroy(uiObject);
+        Destroy(gameObject);
+    }
+}
